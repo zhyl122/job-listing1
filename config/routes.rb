09696fb0e10fa0879bcaab1c2cd341f3b64e6abb
zhouyilin <zhyl9697@gmail.com>
2017-02-04 19:45:36 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :jobs do
+    resources :resumes
+  end
+  root 'jobs#index'
   namespace :admin do
     resources :jobs do
+      resources :resumes
       member do
         post :publish
         post :hide
       end
     end
   end
-
-  resources :jobs do
-    resources :resumes
-  end
-  
-  root 'jobs#index'
 end
